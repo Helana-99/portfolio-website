decea4faf41653e8ecdfbcd936e5100abf77e703
Helana-99 <helananabil299@gmail.com>
2024-01-24 06:19:17 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// var userName = window.prompt('inter your name')
+// document.getElementById('demo').innerHTML = 'welcome: ' + userName
+
+// for (var x = 10; x < 20; x++) {
+
+//     console.log('hello world' + x);
+// }
+
+// var copaya = ''
+// for (var i = 1950; i < 2024; i++) {
+//     copaya += '<option>' + i + '</option>'
+// }
+
+
+// document.getElementById('demo').innerHTML = copaya
+
+// var copaya = ''
+// for (var i = 2024; i < 1950; i--) {
+//     copaya += '<option>' - i - '</option>'
+// }
+
+
+// document.getElementById('demo').innerHTML = copaya
+
+// var carton = ''
+// for (var x = 0 ;x < 100; x++) {
+//     carton += `
+//     <div class="col-md-3">
+//     <img src="images (3).jpg" alt="">
+//     <h1>Lorem, ipsum.</h1>
+//     <p>Lorem ipsum dolor sit amet consectetur.</p>
+// </div>
+// </div>
+// `
+// }
+// document.getElementById('demo').innerHTML = carton
+
+
+for(var i= 0; i<100 ; i++){
+    cartona+=`
+    <div class="col-md-4 my-5">
+    <div class="bg-light p-4 position-relative">
+        <div class="start-50 top-0 translate-middle position-absolute">
+            <i class="fa-solid fa-anchor fa-3x"></i>
+        </div>
+        <h2 class="pt-3">Lorem, ipsum.</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+    </div>
+    
+    `
+    
+}
